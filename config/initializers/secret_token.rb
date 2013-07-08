8728d2244afa6e98c85cd25b5f1cdaaf8547cef3
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-08 08:35:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NotsubliminalRails::Application.config.secret_key_base = '60273f62ccd4c284a19eeb8ad343ac24432872d89c19df07d32de1f016eb19887e0dc2502d33ac669d0cf71a60b281f00f6d29c18f176e0025c1a9923bc3a2cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NotsubliminalRails::Application.config.session_store :cookie_store, key: '_notsubliminal_rails_session'
