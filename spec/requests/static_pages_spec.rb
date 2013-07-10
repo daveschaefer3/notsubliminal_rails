dcdce4ee7afb3e6ddda217995143c56661c66667
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-09 21:50:33 -0700

    Wrote some copy on static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,20 @@ describe "StaticPages" do
       expect(page).to have_content('Not Subliminal')
     end
 
+  end # Home Page
+
+  describe "About Page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
   end
 end
