68bc9362b0a75667e45a3b4b6353c6a1d8430e48
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-10 08:07:20 -0700

    Added proper titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,26 +2,39 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
+    before { visit '/static_pages/home' }
 
     it "should have the content 'Not Subliminal'" do
-      visit '/static_pages/home'
       expect(page).to have_content('Not Subliminal')
+    end
+
+    it "should have the right title" do
+      expect(page).to have_title("Not Subliminal")
     end
 
   end # Home Page
 
   describe "About Page" do
+    before { visit '/static_pages/about' }
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+
+    it "should have the right title" do
+      expect(page).to have_title("Not Subliminal | About")
     end
   end
 
   describe "Contact Page" do
+    before { visit '/static_pages/contact' }
+
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
       expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      expect(page).to have_title("Not Subliminal | Contact")
     end
   end
 end
