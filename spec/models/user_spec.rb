656a1136b472512a56fcbc6a42500b598eb6826b
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-12 15:48:50 -0700

    Added secure passwords and more validations to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", email: "user@example.com",
+  	password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
@@ -10,6 +11,7 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:password_digest) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
@@ -24,5 +26,17 @@ describe User do
   	before { @user.email = " " }
   	it { should_not be_valid }
   end
-  
+
+  describe "when password is not present" do
+  	before do 
+  		@user = User.new(name: "Example User", email: "example@notsubliminal.com",
+  			password: " ", password_confirmation: " ")
+  	end
+  	it { should_not be_valid }
+  end
+
+  describe "when password confirmation doesn't match" do
+  	before { @user.password_confirmation = "mismatch" }
+  	it { should_not be_valid }
+  end
 end
