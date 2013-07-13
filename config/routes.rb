679a82aa26b247c96fff82b53ace7a0b52ed51b4
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-12 17:50:46 -0700

    Added signup form and more tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NotsubliminalRails::Application.routes.draw do
+  resources :users
   get "users/new"
 
   root 'static_pages#home'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,40 +17,82 @@ describe User do
 
   it { should be_valid }
 
-  describe "when name is not present" do
-  	before { @user.name = " " }
-  	it { should_not be_valid }
-  end
+  describe "name" do
+    describe "when name is not present" do
+    	before { @user.name = " " }
+    	it { should_not be_valid }
+    end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
+    describe "when name is too long" do
+      before { @user.name = "a" * 51 }
+      it { should_not be_valid }
+    end
 
-  describe "when name is too short" do
-    before { @user.name = "foob" }
-    it { should_not be_valid }
-  end
+    describe "when name is too short" do
+      before { @user.name = "foob" }
+      it { should_not be_valid }
+    end
+  end # name
 
-  describe "when email is not present" do
-  	before { @user.email = " " }
-  	it { should_not be_valid }
-  end
+  describe "email" do
+    describe "when email is not present" do
+    	before { @user.email = " " }
+    	it { should_not be_valid }
+    end
 
-  describe "when email is not valid" do
+    describe "when email is not valid" do
 
-  end
+    end
 
-  describe "when password is not present" do
-  	before do 
-  		@user = User.new(name: "Example User", email: "example@notsubliminal.com",
-  			password: " ", password_confirmation: " ")
-  	end
-  	it { should_not be_valid }
-  end
+    describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
-  describe "when password confirmation doesn't match" do
-  	before { @user.password_confirmation = "mismatch" }
-  	it { should_not be_valid }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
+  end # email
+
+  describe "password" do
+    describe "when password is not present" do
+    	before do 
+    		@user = User.new(name: "Example User", email: "example@notsubliminal.com",
+    			password: " ", password_confirmation: " ")
+    	end
+    	it { should_not be_valid }
+    end
+
+    describe "when password confirmation doesn't match" do
+    	before { @user.password_confirmation = "mismatch" }
+    	it { should_not be_valid }
+    end
+
+    describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "foob" }
+      it { should be_invalid }
+    end
+
+    describe "return value of authenticate method" do
+      before { @user.save }
+      let(:found_user) { User.find_by(email: @user.email) }
+
+      describe "with valid password" do
+        it { should eq found_user.authenticate(@user.password) }
+      end
+
+      describe "with invalid password" do
+        let(:user_for_invlaid_password) { found_user.authenticate("invalid") }
+
+        it { should_not eq user_for_invlaid_password }
+        specify { expect(user_for_invlaid_password).to be_false }
+      end
+    end # return value of authenticate method
+    
+  end # password
+
+  describe "admin" do
+
+  end # admin?
 end
