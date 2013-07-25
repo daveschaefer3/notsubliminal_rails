6c35294cd8d270754d376fa07958db2be958166c
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-25 14:14:58 -0700

    Sign in users on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:info] = "Thanks for signing up for Not Subliminal! An administrator will review your request."
       redirect_to @user
     else
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -43,6 +43,7 @@ describe "UserPages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign Out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-info', text: 'Thanks') }
       end
