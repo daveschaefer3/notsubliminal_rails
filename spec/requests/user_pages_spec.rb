c45d6e929d50788fcaef16cf079615387183cfa1
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-24 21:27:32 -0700

    Added more signup page tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,6 +18,13 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign Up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid info" do
@@ -30,6 +37,14 @@ describe "UserPages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-info', text: 'Thanks') }
       end
     end
   end # signup
