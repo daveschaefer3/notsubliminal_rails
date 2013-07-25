d861a310af3049b561becaee6d14b13f5ff7fb26
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-25 01:19:01 -0700

    added rspec tests for signing in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,5 +9,30 @@ describe "AuthenticationPages" do
 
     it { should have_content('Sign In') }
     it { should have_title('Sign In') }
-  end
+  end # signin page
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign In" }
+
+      it { should have_title('Sign In') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end # with invalid info
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign In"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Sign Out',    href: signout_path) }
+      it { should_not have_link('Sign In', href: signin_path) }
+    end # with valid information
+  end # signin
 end
