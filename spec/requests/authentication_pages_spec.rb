c70c5c86a3a50fdb43428a48a06e53e3d2dc301b
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-25 14:22:58 -0700

    Added sign out capability

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -38,6 +38,11 @@ describe "AuthenticationPages" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign Out',    href: signout_path) }
       it { should_not have_link('Sign In', href: signin_path) }
+      
+      describe "followed by signout" do
+        before { click_link "Sign Out" }
+        it { should have_link('Sign In') }
+      end
     end # with valid information
   end # signin
 end
