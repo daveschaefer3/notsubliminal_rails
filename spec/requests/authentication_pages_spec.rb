6f45ef4d2a5f30f0b5b46da73ce09034d5d614dd
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-26 03:12:23 -0700

    Require the correct user

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -57,5 +57,21 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(signin_path) }
       end
     end #for non-signed-in users
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@wrong.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_title('Edit Profile') }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
   end # authorization
 end
