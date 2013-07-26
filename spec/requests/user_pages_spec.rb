d7d68e5686aaa917381dcab07a794e12d9224d8c
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-25 21:17:25 -0700

    User updates successful

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -72,6 +72,24 @@ describe "UserPages" do
 
       it { should have_content('error') }
     end # with invalid information
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@new.com"}
+      before do
+        fill_in "Name",       with: new_name
+        fill_in "Email",      with: new_email
+        fill_in "Password",   with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save Changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign Out', href: signout_path) }
+      specify { expect(user.reload.name).to eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end # with valid information
   end # edit
 
 end
