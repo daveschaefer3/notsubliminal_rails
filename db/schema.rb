c8baae4efe77170c8b93c78152a2a58096dbf2e5
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-28 14:08:30 -0700

    Added an explicit default false value for users admin column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130725101930) do
+ActiveRecord::Schema.define(version: 20130728205440) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
     t.string   "remember_token"
     t.datetime "created_at"
     t.datetime "updated_at"
