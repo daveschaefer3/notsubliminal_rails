9ad5c7a8d06932b9e7f538b86f4356f299f077ba
Dave Schaefer <daveschaefer3@gmail.com>
2013-08-31 18:15:41 -0600

    Added artist spec field tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Artist do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @artist = Artist.new(name: "Example Artist", 
+    description: "This in an example description for Example Artist", 
+    email: "exampleartist@test.com", 
+    extlink: "http://www.notsubliminal.com") }
+
+  subject { @artist }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:description) }
+  it { should respond_to(:email) }
+  it { should respond_to(:extlink) }
 end
