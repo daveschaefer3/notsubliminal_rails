9a287de803b0adf6c6bc8f4c135f2a6ebb3c8d6a
Dave Schaefer <daveschaefer3@gmail.com>
2013-08-31 21:15:28 -0600

    Generated Album model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130831234756) do
+ActiveRecord::Schema.define(version: 20130901024044) do
+
+  create_table "albums", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "artists", force: true do |t|
     t.string   "name"
