ea53cd8a81857b5b372a5381f346d9870e24315a
Dave Schaefer <daveschaefer3@gmail.com>
2013-09-02 19:33:38 -0600

    Changed create method to build method.

--- spec/models/album_song_relationship_spec.rb
+++ spec/models/album_song_relationship_spec.rb
@@ -4,7 +4,7 @@ describe AlbumSongRelationship do
 
   let(:albumSong) { FactoryGirl.create(:song) }
   let(:songAlbum) { FactoryGirl.create(:album) }
-  let(:albumSongRelationship) { songAlbum.albumSongRelationships.create(song_id: albumSong.id) }
+  let(:albumSongRelationship) { songAlbum.albumSongRelationships.build(song_id: albumSong.id) }
 
   subject { albumSongRelationship }
 
