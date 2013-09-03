5ba6dc8699498e8b037e005e2f1ae963a850ef2e
Dave Schaefer <daveschaefer3@gmail.com>
2013-09-03 17:21:06 -0600

    Added album views

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,20 +5,28 @@ class AlbumsController < ApplicationController
   end
 
   def create
-    
+    @album = Album.new(album_params)
+    if @album.save
+      flash[:success] = "Album added!"
+      redirect_to root_url
+    else
+      render 'albums/new'
+    end
   end
 
   def show
-
+    @album = Album.find(params[:id])
   end
 
   def edit
+    @album = Album.find(params[:id])
   end
 
   def update
   end
 
   def index
+    @albums = Album.paginate(page: params[:page])
   end
 
   def destroy
