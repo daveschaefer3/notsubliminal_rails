b9a22cc81a3eab8a79528ffe65088bec582f5cc2
Dave Schaefer <daveschaefer3@gmail.com>
2013-09-03 20:58:46 -0600

    Added specs for album views

--- spec/requests/album_pages_spec.rb
+++ spec/requests/album_pages_spec.rb
@@ -9,5 +9,73 @@ describe "Album Pages" do
 
     it { should have_content('Add an Album') }
     it { should have_title(full_title('Add Album')) }
-  end
+
+    describe "add an album" do
+      before { visit new_album_path }
+      let(:submit) { "Add Album" }
+
+      describe "with invalid info" do
+        it "should not create an album" do
+          expect { click_button submit }.not_to change(Album, :count)
+        end
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_title(full_title('Add Album')) }
+          it { should have_content('error') }
+        end
+      end # with invalid info
+
+      describe "with valid info" do
+        before do
+          fill_in "Name",         with: "Example Album"
+          fill_in "Description",  with: "Example Album Description"
+        end
+
+        it "should create an album" do
+          expect { click_button submit }.to change(Album, :count).by(1)
+        end
+
+        describe "after saving the album" do
+          before { click_button submit }
+
+          it { should have_selector('div.alert.alert-success',
+            text: 'Album added!') }
+        end
+      end
+    end # add an album
+  end # new album page
+
+  describe "index page" do
+    let (:user) { FactoryGirl.create(:user) }
+    before (:each) do
+      sign_in user
+      visit albums_path
+    end
+
+    it { should have_title(full_title('Albums')) }
+    it { should have_content('Not Subliminal Albums') }
+
+    describe "pagination" do
+      before(:all) { 35.times { FactoryGirl.create(:album) } }
+      after(:all) { Album.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each album" do
+        Album.paginate(page: 1).each do |album|
+          expect(page).to have_selector('li', text: album.name)
+        end
+      end
+    end # pagination
+  end # index page
+
+  describe "edit album page" do
+
+  end # edit album page
+
+  describe "show album page" do
+
+  end # show album page
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,47 +52,47 @@ describe "UserPages" do
 
     it { should have_content('Sign Up') }
     it { should have_title(full_title('Sign Up')) }
+
+    describe "signup" do
+      before { visit signup_path }
+      let(:submit) { "Create account" }
+
+      describe "with invalid info" do
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_title('Sign Up') }
+          it { should have_content('error') }
+        end
+      end
+
+      describe "with valid info" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirm Password", with: "foobar"
+        end
+
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by(email: 'user@example.com') }
+
+          it { should have_link('Sign Out') }
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-info', text: 'Thanks') }
+        end
+      end
+    end # signup
   end # signup page
-
-  describe "signup" do
-    before { visit signup_path }
-    let(:submit) { "Create account" }
-
-    describe "with invalid info" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
-
-      describe "after submission" do
-        before { click_button submit }
-
-        it { should have_title('Sign Up') }
-        it { should have_content('error') }
-      end
-    end
-
-    describe "with valid info" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirm Password", with: "foobar"
-      end
-
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
-
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
-
-        it { should have_link('Sign Out') }
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-info', text: 'Thanks') }
-      end
-    end
-  end # signup
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
